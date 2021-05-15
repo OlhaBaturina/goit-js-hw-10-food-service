38a0f840880da1efb7b211a0808668226837107d
Olha Baturina <olga.baturina.golovastova@gmail.com>
2021-05-16 00:37:23 +0300

    Создала разметку страницы

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import './styles.css';
 import menu from './menu.json';
 import menuCard from './menu-card.hbs';
 
-const menuRef = document.querySelector('.js-menu');
+console.log(menu);
+console.log(menuCard(menu));
 
+const menuRef = document.querySelector('.js-menu');
+console.log(menuRef);
 const markup = menuCard(menu);
 
-menuRef.insertAdjacentElement('beforeend', markup);
+menuRef.insertAdjacentHTML('beforeend', markup);
