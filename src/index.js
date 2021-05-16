e65d234e2de3af15dc6ff613431b699e8923988b
Olha Baturina <olga.baturina.golovastova@gmail.com>
2021-05-16 16:28:48 +0300

    Исправляю функцию сохранения и возврата данных

--- src/index.js
+++ src/index.js
@@ -2,23 +2,27 @@ import './styles.css';
 import menu from './menu.json';
 import menuCard from './menu-card.hbs';
 
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
 const menuRef = document.querySelector('ul.js-menu');
 const toggleRef = document.querySelector('#theme-switch-toggle');
 const bodyRef = document.querySelector('body');
 
 const markup = menuCard(menu);
 
+// Рендерим разметку
 menuRef.insertAdjacentHTML('beforeend', markup);
 
-const Theme = {
-  LIGHT: 'light-theme',
-  DARK: 'dark-theme',
-};
-
-defaultTheme();
-
+// Слушатель на переключатель тем
 toggleRef.addEventListener('change', checkedOnToggleTheme);
 
+// вызываем функцию с дефолтной темой
+defaultTheme();
+
+// Добавляем переключнение классов
 function checkedOnToggleTheme() {
   if (toggleRef.checked) {
     bodyRef.classList.remove(Theme.LIGHT);
@@ -28,15 +32,19 @@ function checkedOnToggleTheme() {
     bodyRef.classList.add(Theme.LIGHT);
   }
 
+  // сохраняем данные в localStorage
   localStorage.setItem('THEME', bodyRef.classList.value);
-  console.log(bodyRef.classList.value);
 }
 
+// Возвращаем данные чекбокса из localStorage
 function defaultTheme() {
-  if (localStorage.getItem('THEME') !== null) {
-    localStorage.getItem('THEME');
-  }
-  if (bodyRef.classList.value === Theme.DARK) {
-    return (toggleRef.checked = true);
+  bodyRef.setAttribute(
+    'class',
+    localStorage.getItem('THEME') === null ? Theme.LIGHT : localStorage.getItem('THEME'),
+  );
+
+  // Устанавливаем положение переключателя
+  if (localStorage.getItem('THEME') === Theme.DARK) {
+    toggleRef.checked = 'true';
   }
 }
